886fde054cbf57eabfc9e7c143d8225612e14a2d
DenGrif <grifon991@gmail.com>
2024-11-08 17:22:38 +0400

    module_16_5_gotovo

--- module_16_5_OBNOV.py
+++ module_16_5_OBNOV.py
@@ -7,17 +7,21 @@ from typing import List, Optional
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+# Список пользователей
 users = []
 
+# Модель пользователя
 class User(BaseModel):
     id: int
     username: str
     age: int
 
+# Главная страница со списком всех пользователей
 @app.get("/", response_class=HTMLResponse)
 def get_all_users(request: Request):
     return templates.TemplateResponse("users.html", {"request": request, "users": users})
 
+# Страница информации о конкретном пользователе
 @app.get("/user/{user_id}", response_class=HTMLResponse)
 def get_user(request: Request, user_id: int):
     user = next((user for user in users if user.id == user_id), None)
@@ -25,6 +29,7 @@ def get_user(request: Request, user_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
     return templates.TemplateResponse("users.html", {"request": request, "user": user})
 
+# Создание нового пользователя
 @app.post("/user/{username}/{age}", response_model=User)
 def create_user(username: str, age: int):
     new_id = users[-1].id + 1 if users else 1
@@ -32,6 +37,7 @@ def create_user(username: str, age: int):
     users.append(new_user)
     return new_user
 
+# Обновление пользователя
 @app.put("/user/{user_id}/{username}/{age}", response_model=User)
 def update_user(user_id: int, username: str, age: int):
     for user in users:
@@ -41,6 +47,7 @@ def update_user(user_id: int, username: str, age: int):
             return user
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
 
+# Удаление пользователя
 @app.delete("/user/{user_id}", response_model=User)
 def delete_user(user_id: int):
     for index, user in enumerate(users):
